2493bd24f6b29ade10bb05cea088fb6d36ec5110
Tulsi-gohil <gohiltulsi451@gmail.com>
2026-01-02 18:31:24 +0530

    env changes

--- backend/utils/emailserver.js
+++ backend/utils/emailserver.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
 
 const sendEmail = async ({ to, subject, html }) => {
   await transporter.sendMail({
-    from: `"Saul Design" <${process.env.EMAIL_FROM}>`,
+    from: `"Saul Design" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html
