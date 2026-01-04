de5da701889f5623abc4caffb10474e79c5f3f15
Tulsi-gohil <gohiltulsi451@gmail.com>
2026-01-04 11:29:47 +0530

    fix vercel react build

--- backend/models/user.js
+++ backend/models/user.js
@@ -23,4 +23,4 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("UserLogin", userSchema);
+module.exports = mongoose.model("user", userSchema);
