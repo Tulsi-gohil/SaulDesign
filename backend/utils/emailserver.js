29827fdfcb3d3bb0316e42a1062a14007e6ad4a1
Tulsi-gohil <gohiltulsi451@gmail.com>
2026-01-04 11:56:48 +0530

    fix vercel react build

--- backend/utils/emailserver.js
+++ backend/utils/emailserver.js
@@ -1,11 +1,11 @@
 const nodemailer = require("nodemailer");
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",          // ✅ FIX
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Gmail App Password
-  },
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
 });
 
 const sendEmail = async ({ to, subject, html }) => {
@@ -13,7 +13,7 @@ const sendEmail = async ({ to, subject, html }) => {
     from: `"Saul Design" <${process.env.EMAIL_USER}>`,
     to,
     subject,
-    html,
+    html
   });
 };
 
