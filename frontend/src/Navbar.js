5f76e77d4f4de3b1ac768ee9799e699908fc4333
Tulsi-gohil <gohiltulsi451@gmail.com>
2026-01-05 13:49:25 +0530

    fix vercel react build

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -2,26 +2,43 @@ import { Link } from "react-router-dom";
 import "./App.css";
 
 function Navbar() {
-  
-function toggleMenu() {
-  document.getElementById("navMenu").classList.toggle("active");
-}
- 
   return (
-    <nav className="navbar navbar-expand-lg   px-4">
-      <Link className="navbar-brand fw-bold text-white" to="/">SaulDesign</Link>
+    <nav className="navbar navbar-expand-lg custom-navbar px-3">
+      <Link className="navbar-brand fw-bold text-white" to="/">
+        SaulDesign
+      </Link>
 
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+      {/* TOGGLER */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#nav"
+        aria-controls="nav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse justify-content-end" id="nav">
-        <ul className="navbar-nav  ">
-          <li className="nav-item"><Link className="nav-link text-white" to="/">Home</Link></li>
-          <li className="nav-item"><Link className="nav-link text-white" to="/about">About</Link></li>
-           <li className="nav-item"><Link className="nav-link text-white" to="/contact">Contact</Link></li>
-           <li className="nav-item"><Link className="nav-link text-white" to="/Signup">Signup</Link></li>
-           <li className="nav-item"><Link className="nav-link text-white" to="/Login">Login</Link></li>
+      {/* MENU */}
+      <div className="collapse navbar-collapse" id="nav">
+        <ul className="navbar-nav ms-auto text-center">
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/about">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/contact">Contact</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/Signup">Signup</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/Login">Login</Link>
+          </li>
         </ul>
       </div>
     </nav>
