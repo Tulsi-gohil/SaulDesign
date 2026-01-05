b1605a6b178cc57ee7480eb0e2a4dfa191301b77
Tulsi-gohil <gohiltulsi451@gmail.com>
2026-01-05 14:08:38 +0530

    fix email sever

--- backend/utils/emailserver.js
+++ backend/utils/emailserver.js
@@ -1,20 +1,27 @@
 const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // MUST be false for 587
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 const sendEmail = async ({ to, subject, html }) => {
-  await transporter.sendMail({
-    from: `"Saul Design" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html
-  });
+  
+    await transporter.sendMail({
+      from: `"Saul Design" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html
+    });
+  
 };
 
 module.exports = sendEmail;
